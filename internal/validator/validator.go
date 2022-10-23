5864b73e52acd993ed27426aac9b36df30f4c24c
Joanne Yong <2019120152@ub.edu.bz>
2022-10-23 10:20:54 -0700

    edited validator.go to only have necessary methods/functions

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -2,17 +2,6 @@
 
 package validator
 
-import (
-	"net/url"
-	"regexp"
-)
-
-var (
-	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-
-	PhoneRX = regexp.MustCompile(`^\+?\(?[0-9]{3}\)?\s?-\s?[0-9]{3}\s?-\s?[0-9]{4}$`)
-)
-
 // We create a type that wraps our validation errors map
 type Validator struct {
 	Errors map[string]string
@@ -40,17 +29,6 @@ func In(element string, list ...string) bool {
 	return false
 }
 
-// Matches() returns true if a string value matches a specific regex pattern
-func Matches(value string, rx *regexp.Regexp) bool {
-	return rx.MatchString(value)
-}
-
-// ValidWebsite() checks if a string value is a valid web URL
-func ValidWebsite(website string) bool {
-	_, err := url.ParseRequestURI(website)
-	return err == nil
-}
-
 // AddError() adds an error entry to the Errors map
 func (v *Validator) AddError(key, message string) {
 	if _, exists := v.Errors[key]; !exists {
@@ -64,13 +42,4 @@ func (v *Validator) Check(ok bool, key, message string) {
 	if !ok {
 		v.AddError(key, message)
 	}
-}
-
-// Unique() checks that there are no repeating values in the slice
-func Unique(values []string) bool {
-	uniqueValues := make(map[string]bool)
-	for _, value := range values {
-		uniqueValues[value] = true
-	}
-	return len(values) == len(uniqueValues)
 }
