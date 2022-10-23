65e726b8fdad97be85621718af637750b20b2264
Joanne Yong <2019120152@ub.edu.bz>
2022-10-22 20:04:02 -0700

    added css for todo form

--- todo_form/src/Todo.elm
+++ todo_form/src/Todo.elm
@@ -2,7 +2,6 @@ module Todo exposing (User)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-
 type alias User =
     { name : String
     , email : String
@@ -37,42 +36,11 @@ view user =
                 ]
             , div []
                 [ button [ type_ "submit" ]
-                    [ text "Create task" ]
+                    [ text "Create Task" ]
                 ]
             ]
         ]
-formStyle : List (Attribute msg)
-formStyle =
-    [ style "border-radius" "5px"
-    , style "background-color" "#f2f2f2"
-    , style "padding" "20px"
-    , style "width" "300px"
-    ]
 
-
-inputStyle : List (Attribute msg)
-inputStyle =
-    [ style "display" "block"
-    , style "width" "260px"
-    , style "padding" "12px 20px"
-    , style "margin" "8px 0"
-    , style "border" "none"
-    , style "border-radius" "4px"
-    ]
-
-
-buttonStyle : List (Attribute msg)
-buttonStyle =
-    [ style "width" "300px"
-    , style "background-color" "#397cd5"
-    , style "color" "white"
-    , style "padding" "14px 20px"
-    , style "margin-top" "10px"
-    , style "border" "none"
-    , style "border-radius" "4px"
-    , style "font-size" "16px"
-    ]
-    
 main : Html msg
 main =
     view initialModel
